c9af42e3e1fbeda4dad7967ee70dd13252dd95ef
manishadk <manishadhikari777@gmail.com>
2019-05-16 13:15:35 +0545

    delete api

--- index.js
+++ index.js
@@ -35,21 +35,35 @@ application.post('/v1/users',userController.validator, userController.hashGenera
 application.get('/v1/users', function(req,res){
 
 usermodel.User.findAll({
-  attributes: ['username', 'address']
+  attributes: ['id','username', 'address']
 })
 .then(function(result){
-	console.log(result);
-
+// response can be sent from within the middleware 
 res.json(result);
 })
 .catch(function(err){
 
 })
-
-
-
-
 })
+
+application.delete('/v1/users/:id', function(req,res){
+	console.log(req.params.id)
+
+	usermodel.User.destroy({
+		where : { id :req.params.id }
+	})
+	.then(function(){
+res.status(200)
+res.send({"message":"deleted succesfuly"});
+	})
+	.catch(function(err){
+		next({"status":500, "message":"Couldnot delete"})
+	})
+})
+
+// application.get('/v1/users/:id',function(req,res){
+// 	console.log(req.params);
+// })
 
 application.post('/v1/auth',authController.validator,authController.check, function(req,res){
 
@@ -57,35 +71,6 @@ application.post('/v1/auth',authController.validator,authController.check, funct
 	res.send({"message":"You have succesfuly logged in"})
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // application.post('/v1/auth',authController.validator,authController.check,function(req,res){
@@ -132,3 +117,91 @@ application.use(function(err,req,res,next){
 
 application.listen(3001);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// application.get('/v1/users/:id', function(req,res){
+// 	// console.log(req.params.id)
+// 	usermodel.User.findOne({
+// 		where : {id: req.params.id},
+// 		attributes:['username','address']
+// 	})
+// 	.then(function(result){
+// 		res.status(200);
+// 		res.json(result)
+// 	})
+// 	.catch(function(err){
+// console.log(err);
+// 		res.status(500);
+
+// 	})
+// })
+
+// application.put('/v1/users/:id', function(req,res){
+// 	// console.log(req.body)
+// 	// console.log(req.query);
+// 	// console.log(req.params);
+// 	// console.log('req.query');
+// 	usermodel.User.update({
+// 		username:req.body.username
+// 	},
+// 	{where: { id:req.params.id}
+// })
+// })
+
+
+
+
+// application.delete('/v1/users/:id',function(req,res){
+// usermodel.User.destroy({
+// 	where : { id : req.params.id}
+// })
+// })
